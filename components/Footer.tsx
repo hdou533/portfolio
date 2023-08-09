c3bbce84a046fc152d3f9372ae3af8c797b33a46
Sally <sallydou2023@gmail.com>
2023-08-09 23:26:10 +1200

    add header and footer

---
+++ components/Footer.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { navLinks } from './data'
+import Link from 'next/link'
+
+const Footer = () => {
+  return (
+      <footer className='w-full text-white bg-color-bg flex flex-col'>
+          <div className='flex justify-center items-center gap-8'>
+          {navLinks.map((navLink) => 
+              <Link key={navLink.id} href={navLink.address}>
+                  <span>{navLink.title}</span>
+              </Link>
+         )}
+          </div>
+          
+         <hr className='border-t border-pink-600 my-4 mx-auto w-5/6'/>
+   
+          <div className='flex justify-center items-center text-xs mb-24'>
+              <p> A project by Sally &copy; 2023 | Follow me on &nbsp;
+                  <a href="https://github.com/hdou533" target="_blank" className='hover:text-pink-600'>
+                  @github
+                  </a>
+              </p>
+          </div>
+          
+
+    </footer>
+  )
+}
+
+export default Footer
---
+++ components/data.ts
@@ -0,0 +1,28 @@
+type navLinks = {
+    id: string;
+    title: string;
+    address: string;
+}[]
+
+export const navLinks = [
+    {
+        id: "home",
+        title: "Home",
+        address: "/"
+    },
+    {
+        id: "about",
+        title: "About",
+        address: "/about"
+    },
+    {
+        id: "resume",
+        title: "Resume",
+        address: "/resume"
+    },
+    {
+        id: "contact",
+        title: "Contact",
+        address: "/contact"
+    },
+]
