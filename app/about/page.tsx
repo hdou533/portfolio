1892304f898906f0bb1f3fe9fca2b829522f045a
Sally <sallydou2023@gmail.com>
2023-08-11 23:33:00 +1200

    change font globally

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Image from 'next/image'
 
 const About = () => {
   return (
-    <div>about</div>
+    <div className=''>
+
+      <p>Hello there! I'm Sally, a passionate frontend engineer with a strong foundation in IT. I hold a degree in Information Technology from the University of Auckland, where I honed my skills and developed a deep love for crafting exceptional digital experiences.</p>
+      <p>With a year of hands-on experience as a Software Developer, I've had the opportunity to work on diverse projects that have enriched my technical prowess. I thrive on the challenges that coding presents, constantly seeking innovative ways to enhance user interfaces and create seamless interactions.</p>
+
+
+    </div>
   )
 }
 
