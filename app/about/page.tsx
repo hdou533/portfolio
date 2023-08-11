72c7406061e21adfc46c28ca16bf56250e1f3f3c
Sally <sallydou2023@gmail.com>
2023-08-11 23:49:23 +1200

    fix error

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,8 +5,8 @@ const About = () => {
   return (
     <div className=''>
 
-      <p>Hello there! I'm Sally, a passionate frontend engineer with a strong foundation in IT. I hold a degree in Information Technology from the University of Auckland, where I honed my skills and developed a deep love for crafting exceptional digital experiences.</p>
-      <p>With a year of hands-on experience as a Software Developer, I've had the opportunity to work on diverse projects that have enriched my technical prowess. I thrive on the challenges that coding presents, constantly seeking innovative ways to enhance user interfaces and create seamless interactions.</p>
+      <p>Hello there! I&apos;m Sally, a passionate frontend engineer with a strong foundation in IT. I hold a degree in Information Technology from the University of Auckland, where I honed my skills and developed a deep love for crafting exceptional digital experiences.</p>
+      <p>With a year of hands-on experience as a Software Developer, I&apos;ve had the opportunity to work on diverse projects that have enriched my technical prowess. I thrive on the challenges that coding presents, constantly seeking innovative ways to enhance user interfaces and create seamless interactions.</p>
 
 
     </div>
