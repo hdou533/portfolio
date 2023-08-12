446876adb3e96d4cafa5a81a103d204a0c8593f6
Sally <sallydou2023@gmail.com>
2023-08-12 21:46:00 +1200

    style

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -8,9 +8,8 @@ const Contact = () => {
     <div className='w-full h-auto text-justify flex flex-col justify-center lg:items-center gap-8 mb-16 sm:w-[512px]'>
       <div className='flex flex-col gap-8 p-8'>
         <Image src="/message-square-dots-solid-48.png" width={48} height={48} alt='' />
-      </div>
-      <div className='p-8 flex flex-col gap-4'>
-        <p>Hey there!</p>
+      
+        <p className='mt-8'>Hey there!</p>
         <p> I&apos;m thrilled that you&apos;re reaching out. Whether you want to collaborate on a project, chat about the latest tech trends, or simply share your favorite anime recommendations, I&apos;d love to hear from you. Feel free to use the form below to drop me a message. I&apos;ll get back to you as soon as I can.</p>
 
         <p>And hey, if forms aren&apos;t your thing, you can always hit me up directly through my social media handles linked below. Let&apos;s connect and make some magic happen!</p>
--- components/data.ts
+++ components/data.ts
@@ -28,11 +28,39 @@ export const navLinks = [
 ]
 
 export const images = [
-    '/hobby_2.jpg',
-    '/hobby_1.jpg',
-    '/hobby_3.jpg',
-    '/hobby_4.jpg',
-    '/hobby_5.jpg',
-    '/hobby_6.jpg',
-    '/hobby_7.jpg',
+    '/hobby_2.JPG',
+    '/hobby_1.JPG',
+    '/hobby_3.JPG',
+    '/hobby_4.JPG',
+    '/hobby_5.JPG',
+    '/hobby_6.JPG',
+    '/hobby_7.JPG',
+]
+
+
+export const experience = [
+    {
+        title: "Software Developer",
+        organisation: "SmartLeader",
+        year: "2022.06 - 2023.04",
+        desc: "",
+    },
+    {
+        title: "Digital Marketing Specialist",
+        organisation: "New Zealand Business Association",
+        year: "2021.10 - 2022.06",
+        desc: "",
+    },
+    {
+        title: "IT Support",
+        organisation: "Siaola - Lifewise NZ",
+        year: "2021.03 - 2021.05",
+        desc: "",
+    },
+    {
+        title: "Master of Information Technology",
+        organisation: "University of Auckland",
+        year: "2019.07 - 2021.06",
+        desc: "",
+    },
 ]
