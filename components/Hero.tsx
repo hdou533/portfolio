0aaf3f82f00419baa205b6c0da1a65bd4516050a
Sally <sallydou2023@gmail.com>
2023-08-18 00:16:17 +1200

    background fixes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,9 +7,9 @@ const Hero = () => {
   return (
     
     <div className='w-full h-[calc(100vh-4rem)] flex flex-col items-center justify-center p-8 md:flex-row md:h-[calc(100vh-3rem)]'>
-      <div className='absolute top-25 w-4/5 h-3/4 flex flex-wrap items-center gap-8 md:w-1/2 md:gap-2'>
-        <h1 className='text-4xl md:text-5xl'>I&apos;m <b className='text-secondary'>Sally</b> — a Software Developer with a focus on the Frontend and UX Design.</h1>
-        <p className='italic md:text-xl'><span className='text-xl'>&#34;</span>I bring a dynamic blend of frontend expertise and UX design sensibilities to create captivating and user-centric digital experiences. With a passion for crafting seamless interactions and visually appealing interfaces, I thrive at the intersection of technology and design.<span className='text-lg'>&#34;</span></p>
+      <div className='absolute top-16 w-4/5 h-3/4 flex flex-col flex-wrap items-center justify-center gap-20 md:w-1/2'>
+        <h1 className='text-4xl leading-normal lg:text-5xl lg:leading-relaxed'>I&apos;m <b className='text-secondary'>Sally</b> — a Software Developer with a focus on the Frontend and UX Design.</h1>
+        <p className='italic lg:text-xl'><span className='text-xl'>&#34;</span>I bring a dynamic blend of frontend expertise and UX design sensibilities to create captivating and user-centric digital experiences. With a passion for crafting seamless interactions and visually appealing interfaces, I thrive at the intersection of technology and design.<span className='text-lg'>&#34;</span></p>
       </div>
       <div className="area" >
         <ul className="circles">
