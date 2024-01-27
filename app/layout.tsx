de4140d951c1b3339c0c472abd04175837852929
Sally <sallydou2023@gmail.com>
2024-01-27 17:50:51 +1300

    add back to top btn

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,12 @@
+"use client";
 import "./globals.css";
 import type { Metadata } from "next";
 // import { Inter } from 'next/font/google'
 import Header from "../components/layout/Header";
 import Footer from "../components/layout/Footer";
 import { poppins } from "./fonts";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
 // const inter = Inter({ subsets: ['latin'] })
 
@@ -18,6 +21,25 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const [showBackToTop, setShowBackToTop] = useState(false);
+
+  useEffect(() => {
+    const scroll = () => {
+      setShowBackToTop(false);
+      const height = 600;
+      const isPastHeight = window.scrollY > height;
+
+      if (isPastHeight) {
+        setShowBackToTop(true);
+      }
+    };
+    window.addEventListener("scroll", scroll);
+
+    return () => {
+      window.removeEventListener("scroll", scroll);
+    };
+  }, []);
+
   return (
     <html lang="en">
       <body className={poppins.className}>
@@ -25,6 +47,28 @@ export default function RootLayout({
 
         <div className="w-full min-h-screen bg-background text-white flex flex-col justify-start items-center ">
           {children}
+
+          {showBackToTop && (
+            <Link
+              href="#header"
+              className="transition-all ease-in-out  border w-8 h-8 flex flex-col justify-center items-center text-sm rounded-full fixed right-2 bottom-14 sm:right-8 sm:bottom-8 hover:border-secondary hover:shadow-secondary/50 shadow-lg hover:text-secondary"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18"
+                />
+              </svg>
+            </Link>
+          )}
         </div>
 
         <Footer />
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,29 +1,36 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        'background': '#081229',
-        'btn-blue': '#191d3a',
-        'primary': '#0e101c',
-        'secondary': '#bf1650',
-        'light-pink': '#ec5990',
+        background: "#081229",
+        "btn-blue": "#191d3a",
+        primary: "#0e101c",
+        secondary: "#bf1650",
+        "light-pink": "#ec5990",
       },
       fontFamily: {
-        poppins: ['Poppins', 'sans-serif'],
-        londrina: ['Lobster', 'cursive', 'Londrina Outline', 'cursive', 'Poppins', 'sans-serif'],
+        poppins: ["Poppins", "sans-serif"],
+        londrina: [
+          "Lobster",
+          "cursive",
+          "Londrina Outline",
+          "cursive",
+          "Poppins",
+          "sans-serif",
+        ],
       },
       rotate: {
-        '135': '135deg',
+        "135": "135deg",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
